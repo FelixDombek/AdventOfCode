1b5746e3d2e9443807fdfd0689be0f7a41752eb2
Felix Dombek <felics_dombek@hotmail.com>
2023-12-03 22:15:01 +0100

    feat: 2023 door 3

--- app/src/test/kotlin/com/example/adventofcode/AdventOfCode2023.kt
+++ app/src/test/kotlin/com/example/adventofcode/AdventOfCode2023.kt
@@ -4,9 +4,11 @@ import org.junit.Assert.*
 import org.junit.Test
 import java.io.File
 import java.util.Scanner
+import kotlin.math.max
+import kotlin.math.min
 
 fun getInput(day: String): List<String> {
-    val infile = File("advent2023-" + day.padStart(2, '0') + ".input.txt")
+    val infile = File("advent2023-${day.padStart(2, '0')}.input.txt")
     assertTrue("File does not exist: " + infile.absolutePath, infile.isFile)
     return infile.readLines()
 }
@@ -19,6 +21,7 @@ class AdventOfCode2023 {
             "${line.first { it.isDigit() }}${line.last { it.isDigit() }}".toInt()
         }
         println("Door 1.1: $sum")
+        assertEquals(54877, sum)
 
         val sum2 = input.sumOf { line ->
             val re = Regex("one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9")
@@ -44,19 +47,20 @@ class AdventOfCode2023 {
             "$digit1$digit2".toInt()
         }
         println("Door 1.2: $sum2")
+        assertEquals(54100, sum2)
     }
 
     @Test
     fun door02() {
         val input = getInput("2")
         data class Round(val red: Int, val green: Int, val blue: Int) {
-            fun canMatch(numbers: Round) = red <= numbers.red && green <= numbers.green && blue <= numbers.blue
+            fun canMatch(limits: Round) = red <= limits.red && green <= limits.green && blue <= limits.blue
             fun cube() = red * green * blue
         }
         data class Game(val id: Int, val rounds: List<Round>) {
             fun canMatch(numbers: Round) = rounds.all { it.canMatch(numbers) }
             fun minimumPossible() = rounds.fold(Round(0, 0, 0)) { acc, round ->
-                Round(maxOf(acc.red, round.red), maxOf(acc.green, round.green), maxOf(acc.blue, round.blue))
+                Round(max(acc.red, round.red), max(acc.green, round.green), max(acc.blue, round.blue))
             }
         }
         // Game 2: 2 green, 2 blue, 16 red; 14 red; 13 red, 13 green, 2 blue; 7 red, 7 green, 2 blue
@@ -70,13 +74,46 @@ class AdventOfCode2023 {
             }
             Game(id, rounds.toList())
         }
-        val numbers = Round(12, 13, 14)
-        val idSum = games.filter { it.canMatch(numbers) }.sumOf { it.id }
+        val limits = Round(12, 13, 14)
+        val idSum = games.filter { it.canMatch(limits) }.sumOf { it.id }
         println("Door 2.1: $idSum")
         assertEquals(2283, idSum)
 
         val minCubeSum = games.sumOf { it.minimumPossible().cube() }
         println("Door 2.2: $minCubeSum")
         assertEquals(78669, minCubeSum)
+    }
+
+    @Test
+    fun door3() {
+        val input = getInput("3")
+        val sum = input.mapIndexed { lineNum, line ->
+            Regex("\\d+").findAll(line).filter {
+                for (i in max(lineNum - 1, 0)..min(lineNum + 1, input.size - 1)) {
+                    for (j in max(it.range.first - 1, 0)..min(it.range.last + 1, input[lineNum].length - 1)) {
+                        if (!(input[i][j].isDigit() || input[i][j] == '.')) return@filter true
+                    }
+                }
+                false
+            }.sumOf { it.value.toInt() }
+        }.sum()
+        println("Door 3.1: $sum")
+
+        val gears = mutableMapOf<Pair<Int, Int>, MutableList<Int>>()
+        input.forEachIndexed { lineNum, line ->
+            Regex("\\d+").findAll(line).forEach {
+                for (i in max(lineNum - 1, 0)..min(lineNum + 1, input.size - 1)) {
+                    for (j in max(it.range.first - 1, 0)..min(it.range.last + 1, input[lineNum].length - 1)) {
+                        if (input[i][j] == '*') {
+                            gears.putIfAbsent(i to j, mutableListOf())
+                            if (gears[i to j] == null) gears[i to j] = mutableListOf()
+                            gears[i to j]!!.add(it.value.toInt())
+                        }
+                    }
+                }
+            }
+        }
+        val gearSum = gears.filter { it.value.size == 2 }.map { it.value[0] * it.value[1] }.sum()
+        println("Door 3.2: $gearSum")
     }
 }
